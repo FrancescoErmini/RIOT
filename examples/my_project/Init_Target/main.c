a5c2bd5bb1050886f767114036b92a68ab331566
Francesco Ermini <francescoermini8@gmail.com>
2015-04-19 17:55:47 +0200

    PN532 P2P using switching between init_target/initiator, count test

--- examples/my_project/Init_Target/main.c
+++ examples/my_project/Init_Target/main.c
@@ -57,21 +57,24 @@ int pn532_initialization(pn532_t * pn532){
 return 1;
 }
 
-int p2p_initiator(void){
-	uint8_t message[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE };
+int p2p_initiator(pn532_t * pn532, uint8_t * message,uint8_t size_mess){
+	//uint8_t message[] = { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE };
 	uint8_t data[255];
 	uint8_t dlen;
 	uint8_t atr_res[255];
 	uint8_t alen = 0;
-	uint8_t size_mess = sizeof(message);
-
+	//uint8_t size_mess = sizeof(message);
+	if(! pn532_SAM_config(pn532) ){
+					printf("Configuration SAM didn't end well! HALT");
+					while(1);
+				}
 delay(3000);
 
 printf("\n\n\nInitializing PN532 as Initiator!\n\n\n");
 	uint8_t actpass = 0x01;	//Active mode
 	uint8_t br = 0x02;		//424 kbps
 	uint8_t next = 0x00;
-	uint8_t status_initiator = pn532_in_jump_for_dep(&pn532, actpass, br, next, atr_res, &alen );
+	uint8_t status_initiator = pn532_in_jump_for_dep(pn532, actpass, br, next, atr_res, &alen );
 	if( status_initiator != 1 ) {
 		printf("Something went wrong!\n");
 		printf("Here it is response frame to detect the error:\n");
@@ -79,24 +82,25 @@ printf("\n\n\nInitializing PN532 as Initiator!\n\n\n");
 	}else{
 			uint8_t mi = 0;
 			do{
-				pn532_in_data_exchange(&pn532, message, size_mess, 1, &mi, data, &dlen );
+				pn532_in_data_exchange(pn532, message, size_mess, 1, &mi, data, &dlen );
 			}while( mi == 1 );
 			delay(1);
 			mi = 0;
 			//pn532_in_data_exchange( &pn532, message, 0, 1, &mi, data, &dlen );
 
 			puts("Sended. stop.");
+			//return 1;
 	}
 return 1;
 }
 
 
 
-int p2p_target(void){
+int p2p_target(pn532_t * pn532, uint8_t * response,uint8_t *response_size){
 	printf("\nInitialization as target!\n\n");
 	uint8_t data[265];
 	uint8_t dlen = 0;
-	uint8_t response[255];
+	uint8_t responsetmp[255];
 
 		uint8_t nad, did, atr, rats, picc;
 		nad = 0x00;
@@ -104,27 +108,34 @@ int p2p_target(void){
 		atr = 0x00;
 		rats = 0x01;
 		picc = 0x01;
+		if(! pn532_SAM_config(pn532) ){
+						printf("Configuration SAM didn't end well! HALT");
+						while(1);
+					}
 		//pn532_tg_set_parameters( &pn532, nad, did, atr, rats, picc );
-		pn532_set_parameters( &pn532, nad, did, atr, rats, picc );
+		pn532_set_parameters( pn532, nad, did, atr, rats, picc );
 		uint8_t mode = 0x02;	//DEP only
-		uint8_t status_target = pn532_tg_init_as_target(&pn532, mode );
+		uint8_t status_target = pn532_tg_init_as_target(pn532, mode );
 		uint8_t mi = 0;
 		if( status_target != 1) {
 			printf("Initialization didn't successfully finish! HALT\n");
 			delay(10);
 		}else{
 			uint8_t gt[] = { 0x8C, 0x29, 0x0E, 0x04, 0x34, 0xD7, 0x90, 0xE5, 0x61, 0xE0 };
-			pn532_tg_set_general_bytes(&pn532, gt, 10 );
+			pn532_tg_set_general_bytes(pn532, gt, 10 );
 			delay(300);
 			//pn532_tg_get_to_initiator(&pn532, data, &dlen );
-			pn532_tg_get_initiator_command(&pn532, data, &dlen );
+			pn532_tg_get_initiator_command(pn532, data, &dlen );
+			*response_size= dlen-7;
+
 			for(int i=7; i < dlen ;i++ ){
 				printf("\n received[i-7]: %02x",data[i]);
+				responsetmp[i-7]=data[i];
 				response[i-7]=data[i];
 			}
 
 			delay(150);
-			pn532_tg_get_target_status(&pn532);
+			pn532_tg_get_target_status(pn532);
 			puts("received. stop.");
 		}
 return 1;
@@ -133,44 +144,31 @@ int main(void)
 {
 		printf("Welcome to RIOT, this is the Sensor");
 		delay(5);
+		uint8_t out1[] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+		uint8_t out2[] = { 0x11, 0x12, 0x13, 0x14, 0x15 };
+		uint8_t resp1[] ={0x00,0x00,0x00,0x00,0x00 };//up to 255
+		uint8_t resp2[] ={0x00,0x00,0x00,0x00,0x00 };
+		uint8_t resp1_size;
+		uint8_t resp2_size;
 		pn532_initialization(&pn532);
-		 printf("INITIALIZATION FINISH!");
+		p2p_initiator(&pn532,out1,sizeof(out1));
+		p2p_target(&pn532,resp1,&resp1_size);
+		p2p_initiator(&pn532,out2,sizeof(out2));
+    	p2p_target(&pn532,resp2,&resp2_size);
 
-		if(! pn532_SAM_config(&pn532) ){
-				printf("Configuration SAM didn't end well! HALT");
-				while(1);
-			}
+    	for(int i=0;i< (resp1_size);i++)
+    	{
+    		printf("resp1[%i]: %02x",i,resp1[i]);
+    	}
 
-		//p2p_initiator();
-
-		//delay(2000);
-		////pn532_initialization(&pn532);
-
-		p2p_target();
-		//delay(60000);
-		printf("SWITCHING SENSORR!!");
-		 if(! pn532_SAM_config(&pn532) ){
-			 		printf("Configuration SAM didn't end well! HALT");
-			 		while(1);
-			 	}
-
-
-		p2p_initiator();
-		//delay(2000);
-		printf("SWITCHING SENSORR!!");
-
-		 if(! pn532_SAM_config(&pn532) ){
-			 		printf("Configuration SAM didn't end well! HALT");
-			 		while(1);
-			 	}
-
-
-		p2p_target();
-
+    	for(int i=0;i<(resp2_size);i++)
+    	{
+    		printf("resp2[%i]: %02x",i,resp2[i]);
+    	}
 
 for(int i=0;i<10;i++)
 {
-	printf("stoppe.");
+	printf("stoppe[%i].",i);
 }
 
 }
