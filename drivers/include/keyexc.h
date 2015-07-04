4b748dc25bfc399afdd68ea1e7055a652f309657
Francesco Ermini <francescoermini8@gmail.com>
2015-07-04 12:37:54 +0200

    Keyexc: aggiunto Keyexc driver

---
+++ drivers/include/keyexc.h
@@ -0,0 +1,74 @@
+
+
+#ifndef KEYEXC_H_
+#define KEYEXC_H_
+
+#include <stdint.h>
+ #include <stdio.h>
+#include <stdint.h>
+#include "periph_conf.h"
+#include "periph/gpio.h"
+#include "mutex.h"
+#include <pn532.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+
+typedef enum{
+	KEYEXC_IDLE,
+	KEYEXC_PROTO,
+	KEYEXC_802154,
+	KEYEXC_RPL,
+	KEYEXC_COAP,
+
+} keyexc_status_t;
+
+typedef enum {
+	IEEE802154,
+	RPL,
+	COAP,
+}keyexc_proto_t;
+
+typedef enum {
+	GATEWAY,
+	SENSOR,
+}keyexc_node_t;
+
+typedef struct {
+uint8_t channel;
+uint8_t panid[2];
+uint8_t short_address[2];
+uint8_t long_address[8];
+uint8_t key[16];
+}keyexc_802154_t;
+
+typedef struct {
+			keyexc_node_t node_type;
+			uint8_t id;
+			pn532_t * dev;
+			keyexc_proto_t proto;
+			//unsigned long keyexc_timeout;
+			keyexc_status_t status;
+			uint8_t * data;
+			uint16_t rx_count;
+
+
+}keyexc_t;
+
+
+
+
+int keyexc(keyexc_t * keyexc);
+
+void keyexc_init(keyexc_t * keyexc, pn532_t * pn532, keyexc_node_t node_type, uint8_t  id, uint8_t * data);
+
+int timeout_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* keyexc */
+/** @} */
